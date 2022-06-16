93773b2bc70b70f9445d72801d3ac75e453cfdcc
Timo <timo.melnicki@hs-furtwangen.de>
2022-06-16 17:39:47 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,26 +5,4 @@ $('.slider').slick({
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3,
-          dots: true,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1,
-          dots: true,
-        }
-      }
-    ]
   });
