3c8274a6206de73b21aafea98e84a990b9dc5637
Timo <timo.melnicki@hs-furtwangen.de>
2022-06-16 18:33:51 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ $('.slider').slick({
     dots: true,
   });
 
-  /*var pageHeight = window.innerHeight;
+  var pageHeight = window.innerHeight;
   var isAnimating = false;
   document.body.style.transform = 'translate3d(0px,0px,0px)';
   
@@ -43,54 +43,5 @@ $('.slider').slick({
   document.body.addEventListener('transitionend', function(){
     setTimeout(function(){ isAnimating = false; }, 500);
     document.addEventListener('wheel', wheelListener);
-  })*/
+  })
   
-  // Define range of divs
-var currentDiv = 1;
-var minDiv;
-var maxDiv;
-var extraPixel = 0;
-
-var divs = document.querySelectorAll('div[id^="e"]');
-for (var i = 0; i < divs.length; i++) {
-  var id = parseInt(divs[i].id.slice(1), 10);
-  // Check min div
-  if (!minDiv) minDiv = id;
-  if (minDiv > id) minDiv = id;
-  // Check max div
-  if (!maxDiv) maxDiv = id;
-  if (maxDiv < id) maxDiv = id;
-}
-
-
-// Cancel default scroll.
-document.addEventListener('scoll', function(e) {
-  e.preventDefault();
-});
-
-// Use wheel event to simulate scroll.
-document.addEventListener('wheel', function(e) {
-   e.preventDefault();
-
-   // Current scroll.
-  var currScroll = document.body.scrollTop;
-  
-  // Decide next element.
-  if (e.wheelDelta < 0) { // scroll up
-    currentDiv--;
-    if (currentDiv < minDiv) currentDiv = minDiv;
-  } else if (e.wheelDelta > 0) { // scroll down
-    currentDiv++;
-    if (currentDiv > maxDiv) currentDiv = maxDiv;
-  }
-
-  console.log(currentDiv);
-
-  // Check if there's a next/previous div.
-  var goToDiv = document.querySelector('#e' + currentDiv);
-  if (goToDiv) {
-    var newScroll = goToDiv.offsetTop;
-  }
-  
-  document.body.scrollTop = newScroll;
-});
